05e8348c9169f96310470be70571afa667924114
c98101lyl <l.ly@hessel.dk>
2023-02-07 09:32:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IronOcr;
+License.LicenseKey = "Your License Key";
+
+
+
+
+/*My License Key is above*/
+
+
+
+try
+{
+    IronTesseract ocr = new IronTesseract();
+    OcrInput ocrInput = new OcrInput();
+    ocrInput.Add(@"TestImage.png");
+    Console.WriteLine($"Output = {ocr.Read(ocrInput).Text}");
+}
+finally
+{
+    Console.Read();
+}
